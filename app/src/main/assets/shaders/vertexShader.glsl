6eb2e4efc0a2b4404532c62f81a5c025e6291702
JustinWeq <zoomdude111@hotmail.com>
2016-01-26 16:27:23 -0800

    added camera position to vertex shader

--- app/src/main/assets/shaders/vertexShader.glsl
+++ app/src/main/assets/shaders/vertexShader.glsl
@@ -2,6 +2,7 @@ uniform mat4 uMVPMatrix;
 uniform mat4 uMMatrix;
 uniform mat4 uVMatrix;
 uniform mat4 uPMatrix;
+uniform vec3 uCameraPosition;
 attribute vec4 vPosition;
 attribute vec4 aNormal;
 attribute vec2 aTexCoord;
